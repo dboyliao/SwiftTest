a0a9763b10e1ca0d82d3fa2d7e37bd2ab890ee67
dboyliao <qmalliao@gmail.com>
2016-01-10 15:05:07 +0800

    Refactorize module a bit to make the profiling result more accurate.

--- tests/test_color_print.swift
+++ tests/test_color_print.swift
@@ -2,6 +2,15 @@ var oneInt = 1
 var str = "a string"
 var oneFloat = 1.0
 
+colorPrint("Black", color:"black")
+colorPrint("Red", color:"red")
+colorPrint("Green", color:"green")
+colorPrint("Yellow", color:"yellow")
+colorPrint("Blue", color:"blue")
+colorPrint("Magenta", color:"magenta")
+colorPrint("Cyan", color:"cyan")
+colorPrint("White", color:"white")
+
 colorPrint(oneInt, color:"red")
 colorPrint(str, color:"blue")
 colorPrint(oneFloat, color:"green")
